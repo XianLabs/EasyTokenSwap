897d1486e50817d83f870b2ac47146c5c3f69263
XianLabs <44497570+XianLabs@users.noreply.github.com>
2020-06-23 21:29:04 -0400

    removed sliceUint, fixed indenting

--- EasySwap.sol
+++ EasySwap.sol
@@ -1,7 +1,8 @@
-// ----------------------------------------------------------------------------
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-// ----------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// Author: Alex Schwarz
+// Copyright Chimera Digital 2020
+// Smart contract allowing users to send tokens to it, keeping track of how much each person sent.
+// -------------------------------------------------------------------------------------------------
 pragma solidity ^0.5.17;
 
 contract EasySwap {
@@ -10,41 +11,30 @@ contract EasySwap {
   mapping ( uint256 => address ) public holders;
   mapping (uint256 => uint256  ) public holderDeposits;
   uint256 holderCount;
-
-
-    function sliceUint(bytes memory bs, uint start) public pure returns (uint)
-    {
-        require(bs.length >= start + 32, "slicing out of range");
-        uint256 x;
-        assembly {
-            x := mload(add(bs, add(0x20, start)))
-        }
-        return x;
-    }    
     
-    function ByteToInt(bytes32 _number) public pure returns (uint256) {
-        uint256 number = uint256(_number);
-        return number;
-    } 
+  function ByteToInt(bytes32 _number) public pure returns (uint256) {
+      uint256 number = uint256(_number);
+      return number;
+  } 
 
   function deposit(bytes memory data) public payable {
 
-        bytes32 tokendata;
+      bytes32 tokendata;
         
-        data = msg.data;
+      data = msg.data;
 
-        assembly {
-            tokendata := mload(add(data, add(32, 36)))
-        }
+      assembly {
+          tokendata := mload(add(data, add(32, 36)))
+      }
         
-        uint256 tokenAmnt = ByteToInt(tokendata);
-        balances[msg.sender] += tokenAmnt;
-        holders[holderCount] = msg.sender;
-        holderDeposits[holderCount] = tokenAmnt;
-        holderCount += 1;
+      uint256 tokenAmnt = ByteToInt(tokendata);
+      balances[msg.sender] += tokenAmnt;
+      holders[holderCount] = msg.sender;
+      holderDeposits[holderCount] = tokenAmnt;
+      holderCount += 1;
   }
 
    function () external payable {
-        deposit(msg.data);
+      deposit(msg.data);
     }  
 }
